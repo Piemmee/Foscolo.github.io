0042970e1cbbd62bbf020cbff28de993ec101f2a
Piemmee <fedelore06@gmail.com>
2025-03-16 19:12:59 +0100

    Delete site directory

--- site/js/load-scripts.js
+++
@@ -1,7 +0,0 @@
-// Script per caricare dinamicamente gli script necessari
-document.addEventListener('DOMContentLoaded', function() {
-    // Aggiungi lo script add-games-menu.js
-    const script = document.createElement('script');
-    script.src = 'js/add-games-menu.js';
-    document.body.appendChild(script);
-});
